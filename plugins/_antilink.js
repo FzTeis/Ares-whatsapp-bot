bbfbf32914234914acf16ecb9e60942364b06870
Niño Piña <ninopina10@gmail.com>
2024-11-05 21:55:43 -0600

    Actualizar _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,47 +1,20 @@
-const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
+const linkRegex = /(https?:\/\/[^\s]+)/gi
 
-export async function before(m, {conn, isAdmin, isBotAdmin }) {
+export async function before(m, { conn, isAdmin, isBotAdmin }) {
+  if (m.isBaileys && m.fromMe) return !0
+  if (!m.isGroup) return !1
+  let chat = global.db.data.chats[m.chat]
+  let bot = global.db.data.settings[this.user.jid] || {}
+  const isLink = linkRegex.test(m.text)
 
-    if (m.isBaileys && m.fromMe)
-
-        return !0
-
-    if (!m.isGroup) return !1
-
-    let chat = global.db.data.chats[m.chat]
-
-    let bot = global.db.data.settings[this.user.jid] || {}
-
-    const isGroupLink = linkRegex.exec(m.text)
-
-    if (chat.antiLink && isGroupLink && !isAdmin) {
-
-        if (isBotAdmin) {
-
-            const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
-
-            if (m.text.includes(linkThisGroup)) return !0
-
-        }
-
-        await conn.reply(m.chat, `*≡ Enlace Detectado*
-
-            
-
-No permitimos enlaces de otros grupos 
-
-lo siento *@${m.sender.split('@')[0]}*  serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, null, { mentions: [m.sender] } )
-
-        if (isBotAdmin && chat.antiLink) {
-
-                await conn.sendMessage(m.chat, { delete: m.key })
-
-            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-
-        } else if (!chat.antiLink) return //m.reply('')
-
+  if (isLink && !isAdmin) {
+    if (isBotAdmin) {
+      await conn.reply(m.chat, `*≡ Enlace Detectado* No permitimos enlaces en este grupo lo siento *@${m.sender.split('@')[0]}* serás expulsado del grupo`, null, { mentions: [m.sender] })
+      await conn.sendMessage(m.chat, { delete: m.key })
+      await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+    } else {
+      await conn.reply(m.chat, `*≡ Enlace Detectado* No permitimos enlaces en este grupo lo siento *@${m.sender.split('@')[0]}*`, null, { mentions: [m.sender] })
     }
-
-    return !0
-
+  }
+  return !0
 }
